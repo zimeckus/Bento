03ea61d23e626a032ea3273821ba004b1a5cb751
Ziyam Khan <62242732+zimeckus@users.noreply.github.com>
2021-10-10 14:09:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ const CONFIG = {
   twelveHourFormat: true,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
+  greetingMorning: 'Good Morning!',
+  greetingAfternoon: 'Good Afternoon,',
+  greetingEvening: 'Good Evening,',
   greetingNight: 'Go to Sleep!',
 
   // Weather

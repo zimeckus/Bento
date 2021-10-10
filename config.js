11219e5976bf1c98b6949ba36f31d6a3f2e08b3a
Ziyam Khan <62242732+zimeckus@users.noreply.github.com>
2021-10-10 09:45:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,14 +23,14 @@ const CONFIG = {
   greetingNight: 'Go to Sleep!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
+  //weatherKey: 'InsertYourAPIKeyHere123456',
+  //weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
+  //weatherUnit: 'C', // 'F', 'C'
+  //language: 'en', // More languages in https://openweathermap.org/current#multi
 
-  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  //trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+  //defaultLatitude: '37.775',
+  //defaultLongitude: '-122.419',
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
